81627689c248e018ed2e55d6145fbebf96250173
davidkristoffersen <david.kristoffersen@shortcut.io>
2024-06-10 13:07:45 +0200

    Add jitpack config to build inside android dir

--- android/proto/build.gradle.kts
+++ android/proto/build.gradle.kts
@@ -60,7 +60,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.davidkristoffersen"
             artifactId = "asdf-proto-producer"
-            version = "1.1.0"
+            version = "1.1.1"
 
             afterEvaluate {
                 from(components["release"])
