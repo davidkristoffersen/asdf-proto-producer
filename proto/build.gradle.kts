0a42887a28259fcc39fb251e32a222b178185a34
davidkristoffersen <david.kristoffersen@shortcut.io>
2024-06-10 11:30:04 +0200

    Remove manifest application tag from library

--- proto/build.gradle.kts
+++ proto/build.gradle.kts
@@ -95,7 +95,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.davidkristoffersen"
             artifactId = "asdf-proto-producer"
-            version = "1.0.3"
+            version = "1.0.5"
 
             afterEvaluate {
                 from(components["release"])
